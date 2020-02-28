f02d2ea800bd6a987514216244d915878cd8629a
Ninad Bulbule <ninadbb@gmail.com>
2020-02-28 23:55:26 +0530

    Refactored OrganizationLeave to extend BaseModel and fixed null guid issue for creating entity

--- src/main/java/org/webshar/hrms/model/db/OrganizationLeave.java
+++ src/main/java/org/webshar/hrms/model/db/OrganizationLeave.java
@@ -1,57 +1,27 @@
 package org.webshar.hrms.model.db;
 
-import javax.persistence.Entity;
-import java.io.Serializable;
-import java.util.Date;
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Entity
-@Table(name = "organization_leave", uniqueConstraints = {@UniqueConstraint(columnNames = {"id"})})
-public class OrganizationLeave implements Serializable
+@Table(name = "organization_leave")
+@Getter
+@Setter
+public class OrganizationLeave extends BaseModel
 {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "id", nullable = false, unique = true, length = 11)
-  @Getter
-  @Setter
-  private Long id;
-
   @Column(name = "count", nullable = false, unique = true, length = 11)
-  @Getter
-  @Setter
   private Long count;
 
-  @CreationTimestamp
-  @Column(name = "created_at", nullable = false, updatable = false)
-  @Getter
-  @Setter
-  private Date createdAt;
-
-  @UpdateTimestamp
-  @Column(name = "updated_at", nullable = false)
-  @Getter
-  @Setter
-  private Date updatedAt;
-
   @OneToOne
-  @Getter
-  @Setter
   private LeaveType leaveType;
 
   @OneToOne
-  @Getter
-  @Setter
   private Organization organization;
 
   public OrganizationLeave()
