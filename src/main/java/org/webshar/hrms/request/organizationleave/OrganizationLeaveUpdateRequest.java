7c51223a57f784eb0893f5b957dc08349fae2f0e
Ninad <ninadbb@gmail.com>
2020-03-26 18:30:51 +0530

    hrms-22 removed id from patch request for OrganizationLeave

--- src/main/java/org/webshar/hrms/request/organizationleave/OrganizationLeaveUpdateRequest.java
+++ src/main/java/org/webshar/hrms/request/organizationleave/OrganizationLeaveUpdateRequest.java
@@ -1,28 +1,18 @@
 package org.webshar.hrms.request.organizationleave;
 
-import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
-import org.webshar.hrms.constants.ErrorMessageConstants;
+import lombok.ToString;
 
-public class OrganizationLeaveUpdateRequest
-{
+@Getter
+@Setter
+@ToString
+public class OrganizationLeaveUpdateRequest {
 
-  @NotNull(message = ErrorMessageConstants.RESOURCE_PERMISSION_ID_NOT_NULL)
-  @Getter
-  private Long id;
-
-  @Getter
-  @Setter
   private Long leaveTypeId;
 
-  @Getter
-  @Setter
   private Long organizationId;
 
-  @Getter
-  @Setter
   private Long count;
-
 
 }
