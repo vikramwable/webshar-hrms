bcea8193a6726ba7da15818c547b8f3d91103b5a
Ninad <ninadbb@gmail.com>
2020-03-26 18:59:29 +0530

    hrms-22 removed id from patch request for Organization

--- src/main/java/org/webshar/hrms/request/organization/OrganizationUpdateRequest.java
+++ src/main/java/org/webshar/hrms/request/organization/OrganizationUpdateRequest.java
@@ -1,30 +1,23 @@
 package org.webshar.hrms.request.organization;
 
 import javax.validation.constraints.AssertTrue;
-import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.apache.commons.lang3.StringUtils;
 import org.webshar.hrms.constants.ErrorMessageConstants;
 
-public class OrganizationUpdateRequest
-{
+@Getter
+@Setter
+@ToString
+public class OrganizationUpdateRequest {
 
-  @NotNull(message = ErrorMessageConstants.ORGANIZATION_ID_NOT_NULL)
-  @Getter
-  private Long id;
-
-  @Getter
-  @Setter
   private String name;
 
-  @Getter
-  @Setter
   private Boolean isActive;
 
   @AssertTrue(message = ErrorMessageConstants.ORGANIZATION_NAME_NOT_BLANK)
-  private boolean isValidName()
-  {
+  private boolean isValidName() {
     return name == null || StringUtils.isNoneBlank(name);
   }
 }
