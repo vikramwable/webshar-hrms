acd87a046c20aa83008739a87f940c24570469f2
Nikhil Shinde <nikhil.shinde@healthfidelity.com>
2020-03-28 15:23:17 +0530

    Code optimization and cleanup

--- src/main/java/org/webshar/hrms/repository/LeaveAllocationRepository.java
+++ src/main/java/org/webshar/hrms/repository/LeaveAllocationRepository.java
@@ -1,8 +1,10 @@
 package org.webshar.hrms.repository;
 
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.webshar.hrms.model.db.LeaveAllocation;
 
@@ -10,14 +12,7 @@ public interface LeaveAllocationRepository extends
     JpaRepository<LeaveAllocation, Long>
 {
 
-  public List<LeaveAllocation> findByEmployeeId(@Param("employee_id") final Long employee_id);
-
-  public List<LeaveAllocation> findByEmployeeIdAndLeaveTypeIdAndStartDateAndEndDate(
-      @Param("employee_id") final Long employeeId,
-      @Param("leave_type_id") final Long leaveTypeId,
-      @Param("start_date") final Date startDate,
-      @Param("end_date") final Date endDate
-  );
+  public List<LeaveAllocation> findByEmployeeId(@Param("id") final Long id);
 
   public List<LeaveAllocation> findAllByStartDateGreaterThanEqualAndStartDateLessThanEqual(
       @Param("start_date") final Date startDate,
@@ -33,14 +28,25 @@ public interface LeaveAllocationRepository extends
       @Param("start_date") final Date startDate,
       @Param("end_date") final Date endDate
   );
-  public LeaveAllocation findByEmployeeIdAndLeaveTypeId(
-      @Param("employee_id") final Long employeeId,
-      @Param("leave_type_id") final Long leaveTypeId
-  );
 
   public void deleteByEmployeeId(@Param("employee_id") final Long employeeId);
 
   public void deleteByEmployeeIdAndLeaveTypeId(@Param("employee_id") final Long employeeId,
       @Param("leave_type_id") final Long leaveTypeId);
 
+  @Query
+      (" SELECT l " +
+      " FROM LeaveAllocation l " +
+      " WHERE l.employee.id = :employeeId" +
+      " AND l.leaveType.id = :leaveTypeId" +
+      " AND (" +
+      "       (:startDate <= l.startDate AND l.startDate <=  :endDate)" +
+      "       OR (:startDate <= l.endDate AND l.endDate <=  :endDate)" +
+      "       OR (l.startDate <= :startDate AND  :endDate <= l.endDate )" +
+      " )")
+  public LeaveAllocation findGivenTypeOfLeaveAllocatedOrNotInThatDateRange(
+      @Param("employeeId") Long employeeId,
+      @Param("leaveTypeId") Long leaveTypeId,
+      @Param("startDate") LocalDate startDate,
+      @Param("endDate") LocalDate endDate);
 }
--- src/main/java/org/webshar/hrms/repository/LeaveApplicationRepository.java
+++ src/main/java/org/webshar/hrms/repository/LeaveApplicationRepository.java
@@ -1,9 +1,10 @@
 package org.webshar.hrms.repository;
 
+import java.time.LocalDate;
 import java.util.Collection;
-import java.util.Date;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.webshar.hrms.model.db.LeaveApplication;
 
@@ -12,11 +13,11 @@ public interface LeaveApplicationRepository extends JpaRepository<LeaveApplicati
 
   public LeaveApplication findByEmployeeIdAndStartDateAndEndDate(
       @Param("employee_id") final Long employeeId,
-      @Param("start_date") final Date startDate,
-      @Param("end_date") final Date endDate
+      @Param("start_date") final LocalDate startDate,
+      @Param("end_date") final LocalDate endDate
   );
 
-  public List<LeaveApplication> findByEmployeeId(@Param("employee_id") final Long employeeId);
+  public List<LeaveApplication> findByEmployeeId(@Param("id") final Long employeeId);
 
   public List<LeaveApplication> findByEmployeeIdAndLeaveTypeId(
       @Param("employee_id") final Long employeeId,
@@ -34,24 +35,16 @@ public interface LeaveApplicationRepository extends JpaRepository<LeaveApplicati
       @Param("leave_status_id") final Collection<Long> leaveStatusIds
   );
 
-
-//  public List<LeaveApplication> findAllByStartDateGreaterThanEqualAndStartDateLessThanEqualOrEndDateGreaterThanEqualAndEndDateLessThanEqualOrStartDateLessThanEqualAndEndDateGreaterThanEqual(
-//      @Param("start_date") final Date startDate,
-//      @Param("end_date") final Date endDate
-//  );
-
-  public List<LeaveApplication> findAllByStartDateGreaterThanEqualAndStartDateLessThanEqual(
-      @Param("start_date") final Date startDate,
-      @Param("end_date") final Date endDate
-  );
-
-  public List<LeaveApplication> findAllByEndDateGreaterThanEqualAndEndDateLessThanEqual(
-      @Param("start_date") final Date startDate,
-      @Param("end_date") final Date endDate
-  );
-
-  public List<LeaveApplication> findAllByStartDateLessThanEqualAndEndDateGreaterThanEqual(
-      @Param("start_date") final Date startDate,
-      @Param("end_date") final Date endDate
-  );
+  @Query
+      (" SELECT l " +
+          " FROM LeaveApplication l " +
+          " WHERE " +
+          " (" +
+          "       (:startDate <= l.startDate AND l.startDate <=  :endDate)" +
+          "       OR (:startDate <= l.endDate AND l.endDate <=  :endDate)" +
+          "       OR (l.startDate <= :startDate AND  :endDate <= l.endDate )" +
+          " )")
+  public List<LeaveApplication> findLeaveAlreadyAppliedForGivenDateRange(
+      @Param("startDate") LocalDate startDate,
+      @Param("endDate") LocalDate endDate);
 }
